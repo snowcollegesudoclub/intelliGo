c4885cf74cf0ac97f408ed9745324e71c0134299
alexmickelson <alexmickelson96@gmail.com>
2019-10-01 22:26:45 -0600

    added database stuff

---
+++ databaseScripts/dockerdb.ps1
@@ -0,0 +1,1 @@
+docker run --name intelligoDb -p 5432:5432 -e POSTGRES_USER=intelligo -e POSTGRES_PASSWORD=sudoclub -e POSTGRES_DB=intelligodb postgres
---
+++ intelliGo/Data/MySettings.cs
@@ -0,0 +1,38 @@
+﻿using LinqToDB.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace intelliGo.Data
+{
+    public class ConnectionStringSettings : IConnectionStringSettings
+    {
+        public string ConnectionString { get; set; }
+        public string Name { get; set; }
+        public string ProviderName { get; set; }
+        public bool IsGlobal => false;
+    }
+
+    public class MySettings : ILinqToDBSettings
+    {
+        public IEnumerable<IDataProviderSettings> DataProviders => Enumerable.Empty<IDataProviderSettings>();
+
+        public string DefaultConfiguration => "PostgreSQL";
+        public string DefaultDataProvider => "PostgreSQL";
+
+        public IEnumerable<IConnectionStringSettings> ConnectionStrings
+        {
+            get
+            {
+                yield return
+                    new ConnectionStringSettings
+                    {
+                        Name = "PostgreSQL",
+                        ProviderName = "PostgreSQL",
+                        ConnectionString = @"Server=localhost; Port=5432; Database=intelligodb; User ID=intelligo; Password=sudoclub;"
+                    };
+            }
+        }
+    }
+}
