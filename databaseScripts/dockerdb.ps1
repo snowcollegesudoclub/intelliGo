5227662253ad27819a3d73c5fffb9a635c52f2f7
alexmickelson96 <alexmickelson96@gmail.com>
2019-10-03 18:10:04 -0700

    added database scripts

--- databaseScripts/dockerdb.ps1
+++ databaseScripts/dockerdb.ps1
@@ -1,1 +1,6 @@
-docker run --name intelligoDb -p 5432:5432 -e POSTGRES_USER=intelligo -e POSTGRES_PASSWORD=sudoclub -e POSTGRES_DB=intelligodb postgres
+docker run --name intelligoDb \
+    -p 5432:5432 \
+    -e POSTGRES_USER=intelligo \
+    -e POSTGRES_PASSWORD=sudoclub \
+    -e POSTGRES_DB=intelligodb \
+    postgres
